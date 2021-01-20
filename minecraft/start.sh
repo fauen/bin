6908546f4af03d2bf0d171e9ca4042f1e77f9c3b
fauen <daniel@backman.io>
2021-01-20 20:06:40 +0100

    Changed sed command
    - Changed the sed command from -n to -i. Need to investigate why the n
    flag didn't work but hey, now it's working.

--- minecraft/start.sh
+++ minecraft/start.sh
@@ -3,7 +3,7 @@
 #This part will change the motd of the server to the name of the current directory.
 #Reasoning behind this is I always name the directories with modpack name and version number.
 #The theory is that it will help the players on the server to know which version is running.
-sed -n "s/motd.*/motd=${PWD##*/}/" server.properties
+sed -i "s/motd.*/motd=${PWD##*/}/" server.properties
 
 #This is the general starting parameters I use for minecraft, vanilla or modded.
 java -server -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -Xmx6144M -Xms6144M -jar minecraft_or_forge.jar nogui
